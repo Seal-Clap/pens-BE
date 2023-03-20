28c4d8c09b934b85464c8762f647f45298c07619
sysliar <sysliar@gmail.com>
2023-03-20 22:18:39 +0900

    Initial Commit

---
+++ src/test/java/com/example/pens/PensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
