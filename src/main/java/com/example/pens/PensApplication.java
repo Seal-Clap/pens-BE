8faaf70ad1333fc786e44727f68486cbcd918899
sysliar <sysliar@gmail.com>
2023-03-24 19:33:40 +0900

    test: Spring Security 배제
    일단 배제 (인증 과정 개발 전 단계)

--- src/main/java/com/example/pens/PensApplication.java
+++ src/main/java/com/example/pens/PensApplication.java
@@ -2,8 +2,11 @@ package com.example.pens;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class PensApplication {
 
 	public static void main(String[] args) {
