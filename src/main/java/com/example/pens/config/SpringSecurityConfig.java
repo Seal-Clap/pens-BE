32f2e3ae88288d56a77df2c67861da3c9997f46d
sysliar <sysliar@gmail.com>
2023-03-26 16:26:11 +0900

    feat: spring security 기본 설정
    현재 열려있는 경로
    - /images
    - /auth/join
    - /test

---
+++ src/main/java/com/example/pens/config/SpringSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.pens.config;
+
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+@Configuration
+public class SpringSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().disable()
+                .authorizeHttpRequests(request -> request
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                        .requestMatchers("/test", "/images/**", "/auth/join").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout(withDefaults());
+
+        return http.build();
+    }
+}
