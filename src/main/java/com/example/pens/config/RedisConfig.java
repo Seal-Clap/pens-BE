01b4ccbd8bc409e76e665e4a31ca9c11eae934c7
sysliar <sysliar@gmail.com>
2023-04-22 02:32:14 +0900

    chore: redis propertie change

--- src/main/java/com/example/pens/config/RedisConfig.java
+++ src/main/java/com/example/pens/config/RedisConfig.java
@@ -8,9 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Getter
 @Configuration
@@ -21,12 +21,11 @@ public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String host;
 
-    @Value("${spring.cache.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int port;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
     }
-
 }
