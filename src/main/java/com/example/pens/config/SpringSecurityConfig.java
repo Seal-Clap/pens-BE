d10e3f5e301f2861ced431df81b85a1d765368c1
sysliar <sysliar@gmail.com>
2023-05-02 20:26:00 +0900

    feat: test api

--- src/main/java/com/example/pens/config/SpringSecurityConfig.java
+++ src/main/java/com/example/pens/config/SpringSecurityConfig.java
@@ -44,7 +44,7 @@ public class SpringSecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests()
-                .requestMatchers("/images/**", "/users/login", "/users/register", "/users/groups", "/group/**").permitAll()
+                .requestMatchers("/images/**", "/users/login", "/users/register", "/group/**", "/test").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .apply(new JwtSecurityConfig(jwtTokenProvider))
