78440d319ffd8c4ff39e0bf6725d4c79a856e34f
sysliar <sysliar@gmail.com>
2023-05-11 01:27:58 +0900

    feat: voice channel table define

---
+++ src/main/java/com/example/pens/domain/websocket/VoiceChannel.java
@@ -0,0 +1,26 @@
+package com.example.pens.domain.websocket;
+
+import com.example.pens.domain.Group;
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "voice_channel")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VoiceChannel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer channelId;
+
+    @Id
+    @ManyToOne(targetEntity = Group.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "groupId")
+    private Integer groupId;
+
+}
