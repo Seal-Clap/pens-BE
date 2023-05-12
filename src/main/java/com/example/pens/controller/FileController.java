72e08772fb3d93044c6a461dca41c1eb6c1a384e
sysliar <sysliar@gmail.com>
2023-05-12 16:24:30 +0900

    feat: file download api

--- src/main/java/com/example/pens/controller/FileController.java
+++ src/main/java/com/example/pens/controller/FileController.java
@@ -1,10 +1,16 @@
 package com.example.pens.controller;
 
 import com.example.pens.service.AwsS3Service;
+import com.example.pens.util.FileUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,4 +21,19 @@ public class FileController {
     public ResponseEntity uploadFile(@RequestParam("groupId") Integer groupId, @RequestPart(value = "file") MultipartFile multipartFile) {
         return awsS3Service.uploadFile(groupId, multipartFile);
     }
+
+    @GetMapping("/download")
+    public ResponseEntity<byte[]> downloadFile(@RequestParam("groupId") Integer groupId, @RequestParam("fileName") String fileName) throws IOException {
+        String originalFilePath = FileUtil.buildFileName(groupId, fileName);
+        return awsS3Service.downloadFile(originalFilePath);
+    }
+
+    private HttpHeaders buildHeaders(String resourcePath, byte[] data) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentLength(data.length);
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.setContentDisposition(FileUtil.createContentDisposition(resourcePath));
+        return headers;
+    }
+
 }
--- src/main/java/com/example/pens/service/AwsS3Service.java
+++ src/main/java/com/example/pens/service/AwsS3Service.java
@@ -3,22 +3,26 @@ package com.example.pens.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.*;
+import com.amazonaws.util.IOUtils;
 import com.example.pens.domain.CommonResponse;
 import com.example.pens.util.FileUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLEncoder;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -39,13 +43,29 @@ public class AwsS3Service {
         ObjectMetadata objectMetadata = new ObjectMetadata();
         objectMetadata.setContentType(multipartFile.getContentType());
 
-        try (InputStream inputStream = multipartFile.getInputStream()){
+        try (InputStream inputStream = multipartFile.getInputStream()) {
             amazonS3Client.putObject(new PutObjectRequest(bucketName, fileName, inputStream, objectMetadata)
                     .withCannedAcl(CannedAccessControlList.PublicRead));
-        }catch (IOException e) {
+        } catch (IOException e) {
             return new ResponseEntity(new CommonResponse(false, "File Upload Failed"), HttpStatus.FORBIDDEN);
         }
         return new ResponseEntity(new CommonResponse(true, amazonS3Client.getUrl(bucketName, fileName).toString()), HttpStatus.OK);
+    }
+
+    public ResponseEntity<byte[]> downloadFile(String resourcePath) throws IOException {
+
+        S3Object s3Object = amazonS3Client.getObject(new GetObjectRequest(bucketName, resourcePath));
+        S3ObjectInputStream inputStream = s3Object.getObjectContent();
+        byte[] bytes = IOUtils.toByteArray(inputStream);
+        String fileName = URLEncoder.encode(resourcePath, "UTF-8").replaceAll("\\+", "%20");
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        httpHeaders.setContentLength(bytes.length);
+        httpHeaders.setContentDispositionFormData("attachment", fileName);
+
+        return new ResponseEntity<>(bytes, httpHeaders, HttpStatus.OK);
+
+
     }
 
 
