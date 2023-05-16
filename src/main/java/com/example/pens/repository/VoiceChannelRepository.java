25d74b187af2f77e1ab91f17f46db3bb06927739
sysliar <sysliar@gmail.com>
2023-05-16 23:17:23 +0900

    feat: voice channel create, list api

--- src/main/java/com/example/pens/repository/VoiceChannelRepository.java
+++ src/main/java/com/example/pens/repository/VoiceChannelRepository.java
@@ -4,6 +4,10 @@ import com.example.pens.domain.websocket.VoiceChannel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.nio.channels.Channel;
+import java.util.List;
+
 @Repository
 public interface VoiceChannelRepository extends JpaRepository<VoiceChannel, Integer> {
+    public List<VoiceChannel> findVoiceChannelsByGroup_GroupId(Integer groupId);
 }
--- src/main/java/com/example/pens/service/VoiceChannelService.java
+++ src/main/java/com/example/pens/service/VoiceChannelService.java
@@ -1,10 +1,14 @@
 package com.example.pens.service;
 
+import com.example.pens.domain.websocket.VoiceChannel;
 import org.hibernate.annotations.Any;
 import org.springframework.http.ResponseEntity;
 
+import java.nio.channels.Channel;
+import java.util.List;
 import java.util.Map;
 
 public interface VoiceChannelService {
-    public ResponseEntity create(Map<String, Any> map);
+    public ResponseEntity create(Integer groupId, String channelName);
+    public ResponseEntity getChannels(Integer groupId);
 }
