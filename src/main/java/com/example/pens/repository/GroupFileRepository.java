32f7679652056c78795c2ca0bda06db0bfdc34c4
chlwlsguszz <82369632+chlwlsguszz@users.noreply.github.com>
2023-05-24 19:34:11 +0900

    chore: groupFile 업로드 방식 변경
    이름이 같을 경우 기존의 것 update

--- src/main/java/com/example/pens/repository/GroupFileRepository.java
+++ src/main/java/com/example/pens/repository/GroupFileRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface GroupFileRepository extends JpaRepository<GroupFile, Integer> {
     List<GroupFile> findGroupFileByGroup(Group group);
+
+    Optional<GroupFile> findByFileName(String originalFilename);
 }
